271f4bb3f42680c40f63186def41b58c38899f97
Admin <preciouspeter3173@gmail.com>
2023-01-14 12:32:41 +0100

    click listener activated

--- app/src/main/java/com/example/bianu/MainActivity.java
+++ app/src/main/java/com/example/bianu/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.bianu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.bianu.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        binding.click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+        setContentView(binding.getRoot());
     }
 }
